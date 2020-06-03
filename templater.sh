e986906060a25bf3890e08e85cba4d52c2a30d1d
Maziar Aboualizadehbehbahani <mbooali@gmail.com>
2020-06-03 14:46:25 -0500

    Update templater.sh

--- templater.sh
+++ templater.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
+#!/bin/bash	#!/bin/bash
+#	
+# Very simple templating system that replaces {{VAR}} by the value of $VAR.	# Very simple templating system that replaces {{VAR}} by the value of $VAR.
+# Supports default values by writing {{VAR=value}} in the template.	# Supports default values by writing {{VAR=value}} in the template.
+#	
+# Copyright (c) 2017 Sébastien Lavoie	
+# Copyright (c) 2017 Johan Haleby	
+#	
+# Permission is hereby granted, free of charge, to any person obtaining a copy	
+# of this software and associated documentation files (the "Software"), to deal	
+# in the Software without restriction, including without limitation the rights	
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	
+# copies of the Software, and to permit persons to whom the Software is	
+# furnished to do so, subject to the following conditions:	
+#	
+# The above copyright notice and this permission notice shall be included in all	
+# copies or substantial portions of the Software.	
+#	
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,	
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE	
+# SOFTWARE.	
 #
-# Very simple templating system that replaces {{VAR}} by the value of $VAR.
-# Supports default values by writting {{VAR=value}} in the template.
-#
-# Copyright (c) 2017 Sébastien Lavoie
-# Copyright (c) 2017 Johan Haleby
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# See: https://github.com/johanhaleby/bash-templater
-# Version: https://github.com/johanhaleby/bash-templater/commit/5ac655d554238ac70b08ee4361d699ea9954c941
-
-# Replaces all {{VAR}} by the $VAR value in a template file and outputs it
-
 readonly PROGNAME=$(basename $0)
 
 config_file="<none>"
 print_only="false"
 silent="false"
 
-usage="${PROGNAME} [-h] [-d] [-f] [-s] -- 
+usage="${PROGNAME} [-h] [-d] [-f] [-s] --
 
 where:
     -h, --help
@@ -48,13 +44,13 @@ where:
         Don't print warning messages (for example if no variables are found)
 
 examples:
-    VAR1=Something VAR2=1.2.3 ${PROGNAME} test.txt 
+    VAR1=Something VAR2=1.2.3 ${PROGNAME} test.txt
     ${PROGNAME} test.txt -f my-variables.txt
     ${PROGNAME} test.txt -f my-variables.txt > new-test.txt"
 
-if [ $# -eq 0 ]; then
+if [[ $# -eq 0 ]]; then
   echo "$usage"
-  exit 1    
+  exit 1
 fi
 
 if [[ ! -f "${1}" ]]; then
@@ -65,14 +61,14 @@ fi
 
 template="${1}"
 
-if [ "$#" -ne 0 ]; then
-    while [ "$#" -gt 0 ]
+if [[ "$#" -ne 0 ]]; then
+    while [[ "$#" -gt 0 ]]
     do
         case "$1" in
         -h|--help)
             echo "$usage"
             exit 0
-            ;;        
+            ;;
         -p|--print)
             print_only="true"
             ;;
@@ -99,21 +95,21 @@ fi
 vars=$(grep -oE '\{\{[A-Za-z0-9_]+\}\}' "${template}" | sort | uniq | sed -e 's/^{{//' -e 's/}}$//')
 
 if [[ -z "$vars" ]]; then
-    if [ "$silent" == "false" ]; then
+    if [[ "$silent" == "false" ]]; then
         echo "Warning: No variable was found in ${template}, syntax is {{VAR}}" >&2
     fi
 fi
 
 # Load variables from file if needed
-if [ "${config_file}" != "<none>" ]; then
+if [[ "${config_file}" != "<none>" ]]; then
     if [[ ! -f "${config_file}" ]]; then
       echo "The file ${config_file} does not exists" >&2
-      echo "$usage"      
+      echo "$usage"
       exit 1
     fi
 
     source "${config_file}"
-fi    
+fi
 
 var_value() {
     eval echo \$$1
@@ -126,13 +122,13 @@ replaces=""
 # You can even reference variables defined in the template before
 defaults=$(grep -oE '^\{\{[A-Za-z0-9_]+=.+\}\}' "${template}" | sed -e 's/^{{//' -e 's/}}$//')
 
-for default in $defaults; do
+for default in ${defaults}; do
     var=$(echo "$default" | grep -oE "^[A-Za-z0-9_]+")
-    current=`var_value $var`
+    current=`var_value ${var}`
 
     # Replace only if var is not set
     if [[ -z "$current" ]]; then
-        eval $default
+        eval ${default}
     fi
 
     # remove define line
@@ -141,29 +137,29 @@ for default in $defaults; do
 $current"
 done
 
-vars=$(echo $vars | sort | uniq)
+vars=$(echo ${vars} | sort | uniq)
 
 if [[ "$print_only" == "true" ]]; then
-    for var in $vars; do
-        value=`var_value $var`
+    for var in ${vars}; do
+        value=`var_value ${var}`
         echo "$var = $value"
     done
     exit 0
 fi
 
 # Replace all {{VAR}} by $VAR value
-for var in $vars; do
-    value=$(var_value $var | sed -e "s;\&;\\\&;g" -e "s;\ ;\\\ ;g") # '&' and <space> is escaped 
+for var in ${vars}; do
+    value=$(var_value ${var} | sed -e "s;\&;\\\&;g" -e "s;\ ;\\\ ;g") # '&' and <space> is escaped
     if [[ -z "$value" ]]; then
-        if [ $silent == "false" ]; then
+        if [[ ${silent} == "false" ]]; then
             echo "Warning: $var is not defined and no default is set, replacing by empty" >&2
         fi
     fi
 
     # Escape slashes
     value=$(echo "$value" | sed 's/\//\\\//g');
-    replaces="-e 's/{{$var}}/${value}/g' $replaces"    
+    replaces="-e 's/{{$var}}/${value}/g' $replaces"
 done
 
-escaped_template_path=$(echo $template | sed 's/ /\\ /g')
-eval sed $replaces "$escaped_template_path"
+escaped_template_path=$(echo ${template} | sed 's/ /\\ /g')
+eval sed ${replaces} "${escaped_template_path}"
